008a43c2af9136ffb351381715401c1bd33cc433
Kiefer Bairstow <bairstow@usf.edu>
2025-09-26 18:30:57 -0400

    calculated overtime pay

--- cc_5.js
+++ cc_5.js
@@ -7,19 +7,33 @@ const employees = [
     {name: "Sarah", hourlyRate: 30, hoursWorked: 45},
     {name: "Stefon", hourlyRate: 20, hoursWorked: 20},
     {name: "Alec", hourlyRate: 22, hoursWorked: 25},
-    {name: "Gabe", hourlyRate: 18, hoursWorked: 15},
+    {name: "Gabe", hourlyRate: 18, hoursWorked: 47},
 ]
 
 // Create function calculating base pay
 
 function calculateBasePay(rate, hours) {
     if (hours > 40) {
-        hours = 40;
+        hours = 40;     // if hours over 40 equal 40
 }
-return rate * hours
+    return rate * hours
 }
 
 for (const employee of employees) {
     let pay = calculateBasePay(employee.hourlyRate, employee.hoursWorked);
-    console.log(`${employee.name}'s base pay is : $${pay}`);  
+//    console.log(`${employee.name}'s base pay is : $${pay}`);  
 }
+
+// Calculate overtime pay
+
+function calculateOvetimePay(rate, hours) {
+      let overtimePay = 0;
+    if (hours > 40){
+      let overtimeHours = hours - 40;
+      overtimePay = overtimeHours * (rate * 1.5);
+    }
+    return overtimePay;
+}
+
+// Calculate taxes
+
