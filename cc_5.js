8d0ec9c6288f1c3cdb4519ee8800f546b6df9a9f
Kiefer Bairstow <bairstow@usf.edu>
2025-09-26 17:52:51 -0400

    Create employee array

---
+++ cc_5.js
@@ -0,0 +1,15 @@
+// Coding Challenge 05
+
+// Create an array containing emplyee objects
+
+const employees = [
+    {name: "Jimmy", hourlyRate: 25, hoursWorked: 30},
+    {name: "Sarah", hourlyRate: 30, hoursWorked: 45},
+    {name: "Stefon", hourlyRate: 20, hoursWorked: 20},
+    {name: "Alec", hourlyRate: 22, hoursWorked: 25},
+    {name: "Gabe", hourlyRate: 18, hoursWorked: 15},
+]
+
+function calculateBasePay(params) {
+    
+}
